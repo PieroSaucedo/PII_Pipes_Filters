13716b415766397d0f60149261bfdcd54a84a73c
PieroSaucedo <pierosaucedo@correo.ucu.edu.uy>
2021-10-20 11:33:49 -0300

    Ejercicio Parte 1

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -8,6 +8,16 @@ namespace CompAndDel
     {
         static void Main(string[] args)
         {
+        PictureProvider provider = new PictureProvider();
+        IPicture picture = provider.GetPicture(@".\luke.jpg");
+        IPipe pipeNull = new PipeNull();
+        IFilter filterNegative = new FilterNegative();
+        IPipe pipeSerialFirst = new PipeSerial(filterNegative, pipeNull);
+        IFilter filterGreyscale = new FilterGreyscale();
+        IPipe pipeSerialSecond = new PipeSerial(filterGreyscale, pipeSerialFirst);
+
+        IPicture final = pipeSerialSecond.Send(picture);
+        provider.SavePicture(final, @".\img.jpg");
         }
     }
 }
