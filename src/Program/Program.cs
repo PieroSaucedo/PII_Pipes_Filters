dbb1d56e5e1f6cb0f40f3620c2bcc4d408669f44
PieroSaucedo <pierosaucedo@correo.ucu.edu.uy>
2021-10-20 12:46:59 -0300

    Ejercicio Parte 2

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -8,6 +8,8 @@ namespace CompAndDel
     {
         static void Main(string[] args)
         {
+        /* //EJERCICIO 1 (Quitar símbolos de comentarios para ejecutar)
+        
         PictureProvider provider = new PictureProvider();
         IPicture picture = provider.GetPicture(@".\luke.jpg");
         IPipe pipeNull = new PipeNull();
@@ -18,6 +20,24 @@ namespace CompAndDel
 
         IPicture final = pipeSerialSecond.Send(picture);
         provider.SavePicture(final, @".\img.jpg");
+        
+        */
+
+        PictureProvider provider2 = new PictureProvider();
+        IPicture picture2 = provider2.GetPicture(@".\luke.jpg");
+        
+        IPipe pipeNull2 = new PipeNull();
+        
+        IFilter filterNegative2 = new FilterNegative();
+        IPipe pipeSerialFirst2 = new PipeSerial(filterNegative2, pipeNull2);
+        IPicture filterNegativePicture = pipeSerialFirst2.Send(picture2);
+        provider2.SavePicture(filterNegativePicture, @".\lukeNegativeFilter.jpg");
+        
+        IFilter filterGreyscale2 = new FilterGreyscale();
+        IPipe pipeSerialSecond2 = new PipeSerial(filterGreyscale2, pipeNull2);
+        IPicture filterGreyscalePicture = pipeSerialSecond2.Send(picture2);
+        provider2.SavePicture(filterGreyscalePicture, @".\lukeGreyscaleFilter.jpg");
+
         }
     }
 }
